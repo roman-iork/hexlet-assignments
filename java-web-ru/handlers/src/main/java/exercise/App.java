4fdee209e598569cf4469d6947709b6ac05d2569
@hexlet/cli <support@hexlet.io>
2024-05-26 22:39:39 +0400

    submit java-web-ru/handlers

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -21,6 +21,5 @@ public final class App {
     public static void main(String[] args) {
         Javalin app = getApp();
         app.start(7070);
-//        System.out.println(Data.getDomains());
     }
 }
